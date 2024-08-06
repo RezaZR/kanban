baca89380ec810c8ba2008ebbf53d23e227562c9
RezaZR <rezazramadhan@gmail.com>
2024-08-07 00:42:47 +0700

    feat: add staging list

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+import stagesResponse from "./jsons/stages.json";
+
+const stages = ref(stagesResponse.results);
 </script>
 
 <template>
-  <div>
-    yep
+  <div class="staging">
+    <ul class="list">
+      <li class="item" v-for="stage in stages">
+        <h2 
+          class="title"
+          :style="{'--header-color': stage.header_color }"
+        >
+          {{ stage.name }}
+        </h2>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
+.staging {
+  background-color: #dcdcdc;
+}
+
+.list {
+  display: flex;
+  gap: 16px;
+  overflow: auto;
+  height: 100vh;
+}
+
+.item {
+  background-color: #f5f5f5;
+  border-radius: 4px;
+  flex: 1 0 300px;
+}
+
+.title {
+  font-size: 16px;
+  font-weight: 600;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 8px;
+  background-color: var(--header-color);
+  color: #5a5a5a;
+}
 </style>
